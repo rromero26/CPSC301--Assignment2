33ed82d8188bad3722cde1be54e88c0f73d41c28
Ryan R <rromero26@csu.fullerton.edu>
2019-02-28 12:41:14 -0800

    Initalizing files

--- pay.cpp
+++ pay.cpp
@@ -1,1 +1,11 @@
 #include "person.cpp"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+main(){
+
+  return 0;
+}
--- person.cpp
+++ person.cpp
@@ -1,1 +1,31 @@
 #include "person.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+Person();
+
+ //---------------------------------------------------------------
+void   setFirstName(string fName);
+string getFirstName();
+ //---------------------------------------------------------------
+void   setLastName(string lName);
+string getLastName();
+ //---------------------------------------------------------------
+void   setEmployeeId(int id);
+int    getEmployeeId();
+ //---------------------------------------------------------------
+void   setCompanyName(string coName);
+string getCompanyName();
+ //---------------------------------------------------------------
+void   setPayRate(float rate);
+float  getPayRate();
+ //---------------------------------------------------------------
+void   setHoursWorked(float hours);
+float  getHoursWorked();
+ //---------------------------------------------------------------
+float  totalPay();
+ //---------------------------------------------------------------
+string fullName();
