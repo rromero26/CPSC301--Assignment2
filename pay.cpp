fbf6d031dc9732eb96abcf9585fa586ba1e43b7f
Ryan R <rromero26@csu.fullerton.edu>
2019-03-05 11:47:13 -0800

    Checkpoint

--- pay.cpp
+++ pay.cpp
@@ -2,12 +2,80 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
 
+//-------------FUNCTIONS--------------------------------------------------------
+void readData(string myString = "input.txt", vector& myVect){
+  fstream myFile;                         //opens file
+  myFile.open(myString);
+
+  if(!myFile) {                           //check if file open properly
+    cout << "Error: data.txt could not open. Closing Program." << endl;
+    exit(1);
+  }
+
+  string  fName;
+  string  lName;
+  int     id;
+  string  coName;
+  float   rate;
+  float   hours;
+  int     i = 0;
+        //----- LOOP:Reading/Saving Data ------
+  while(!myFile.eof()){
+    myFile >> fName;
+    myFile >> lName;
+    myFile >> id;
+    myFile >> coName;
+    myFile >> hours;
+    myFile >> rate;
+
+    myVect[i].setFirstName(fName);
+    myVect[i].setLastName(lName);
+    myVect[i].setEmployeeId(id);
+    myVect[i].setCompanyName(coName);
+    myVect[i].setPayRate(rate);
+    myVect[i].setHoursWorked(hours);
+    myVect.push_back(Person);
+    i++;
+    }
+
+    myFile.close();
+};
+//---------------------------------------------------------------
+void getCompanies(vector& EmployeeVect, vector& myCompanyVect){
+  int counter = 0;
+
+  while (counter < EmployeeVect.size()){
+    myCompanyVect.push_back(EmployeeVect[counter].getCompanyName());
+    counter++;
+  }
+};
+//---------------------------------------------------------------
+TYPE printHighestPaid(---){
+  int counter = 0;
+
+  while(/* CONDITION */){
+    //CODE
+  }
+};
+//---------------------------------------------------------------
+TYPE seporateAndSave(---){
+
+};
+//-----------------------END OF FUNCTIONS--------------------------------------
 main(){
   vector <Person> employees = {};
-  vector <string> companyNames
+  vector <string> companyNames;
+
+  readData("input.txt", employees);
+
+  getCompanies(employees, companyNames);
+
+
+
   return 0;
 }
--- person.cpp
+++ person.cpp
@@ -1,6 +1,7 @@
 #include "person.h"
 #include <fstream>
 #include <string>
+#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
@@ -17,38 +18,38 @@ Person::Person(){
  //---------------------------------------------------------------
 void Person::setFirstName(string fName){
   firstName = fName;
-}
+};
 string Person::getFirstName(){
   return firstName;
-}
+};
  //---------------------------------------------------------------
 void Person::setLastName(string lName){
   lastName = lName;
-}
+};
 string Person::getLastName(){
   return lastName;
-}
+};
  //---------------------------------------------------------------
 void Person::setEmployeeId(int id){
   employeeID = id;
-}
+};
 int Person::getEmployeeId(){
   return employeeID;
-}
+};
  //---------------------------------------------------------------
 void Person::setCompanyName(string coName){
   companyName = coName;
-}
+};
 string Person::getCompanyName(){
   return companyName;
-}
+};
  //---------------------------------------------------------------
 void Person::setPayRate(float rate){
   payRate = rate;
-}
+};
 float Person::getPayRate(){
   return payRate;
-}
+};
  //---------------------------------------------------------------
 void Person::setHoursWorked(float hours){
   hoursWorked = hours;
@@ -59,21 +60,8 @@ float Person::getHoursWorked(){
  //---------------------------------------------------------------
 float Person::totalPay(){
   return (getPayRate() * hoursWorked())
-}
+};
  //---------------------------------------------------------------
 string Person::fullName(){
   return (getFirstName() + " " + getLastName())
-}
-
-TYPE readData(---){
-
-}
-TYPE getCompanies(---){
-
-}
-TYPE printHighestPaid(---){
-
-}
-TYPE seporateAndSave(---){
-  
-}
+};
