489d5d1de9d36793cb61692c27d513928a46302b
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2019-04-09 12:40:28 -0700

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,67 +1,68 @@
-#include "person.h"
+#include "Person.h"
 #include <fstream>
 #include <string>
-#include <stdlib.h>
+#include <math.h>
 #include <iostream>
 
 using namespace std;
 
-Person::Person(){
-  firstName = "  N/A  ";
-  lastName = " ";
-  employeeID = 0;
-  companyName = "  N/A  ";
-  hoursWorked = 0.0;
-  payRate = 0.0;
-};
+  Person::Person(){
+      lastName = " ";
+      firstName = " N/A ";
+      payRate = 0.0;
+      hoursWorked = 0.0;
+  }
 
- //---------------------------------------------------------------
-void Person::setFirstName(string fName){
-  firstName = fName;
-};
-string Person::getFirstName(){
-  return firstName;
-};
- //---------------------------------------------------------------
-void Person::setLastName(string lName){
-  lastName = lName;
-};
-string Person::getLastName(){
-  return lastName;
-};
- //---------------------------------------------------------------
-void Person::setEmployeeId(int id){
-  employeeID = id;
-};
-int Person::getEmployeeId(){
-  return employeeID;
-};
- //---------------------------------------------------------------
-void Person::setCompanyName(string coName){
-  companyName = coName;
-};
-string Person::getCompanyName(){
-  return companyName;
-};
- //---------------------------------------------------------------
-void Person::setPayRate(float rate){
-  payRate = rate;
-};
-float Person::getPayRate(){
-  return payRate;
-};
- //---------------------------------------------------------------
-void Person::setHoursWorked(float hours){
-  hoursWorked = hours;
-};
-float Person::getHoursWorked(){
-  return hoursWorked;
-};
- //---------------------------------------------------------------
-float Person::totalPay(){
-  return (getPayRate() * getHoursWorked());
-};
- //---------------------------------------------------------------
-string Person::fullName(){
-  return (getFirstName() + " " + getLastName());
-};
+  Person::Person(string fName, string lName, float Rate, float hrWorked){
+      firstName = fName;
+      lastName = lName;
+      payRate = Rate;
+      hoursWorked = hrWorked;
+}
+
+
+  //---------------------------------------------------------------
+  void Person::setLastName(string lName){
+      lastName = lName;
+  }
+  string Person::getLastName(){
+      return lastName;
+  }
+  //---------------------------------------------------------------
+
+  void Person::setFirstName(string fName){
+      firstName = fName;
+  }
+  string Person::getFirstName(){
+      return firstName;
+  }
+  //---------------------------------------------------------------
+
+  void Person::setPayRate(float rate){
+      payRate = rate;
+  }
+  float Person::getPayRate(){
+      return payRate;
+  }
+  //---------------------------------------------------------------
+
+  void Person::setHoursWorked(float hours){
+      hoursWorked = hours;
+  }
+  float Person::getHoursWorked(){
+      return hoursWorked;
+  }
+  //---------------------------------------------------------------
+
+  float Person::totalPay(){
+      float myVal = hoursWorked * payRate;
+      myVal = floorf(myVal * 100) / 100;
+      return myVal;
+  }
+  //---------------------------------------------------------------
+
+  string Person::fullName(){
+      string theFullName;
+      theFullName = firstName + " " + lastName;
+      return theFullName;
+  }
