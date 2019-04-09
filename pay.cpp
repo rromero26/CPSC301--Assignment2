78b3dd37939c7d7b535fa206638bcba0bdb88231
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2019-04-09 12:41:06 -0700

    Update pay.cpp

--- pay.cpp
+++ pay.cpp
@@ -1,134 +1,67 @@
-#include "person.cpp"
 #include <fstream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <stdlib.h>
 #include <iostream>
-#include <iomanip>
+#include <stdlib.h>
+#include "Person.cpp"
+
 using namespace std;
+//-----------------------------------------------------------------------------------------
+int readData(vector<Person> &myVector, int size){    // WILL RETURN A INT counter OF NUM OF EMPLOYEES
 
-//-------------FUNCTIONS--------------------------------------------------------------------------------------
-void readData(string myString, vector<Person>  &myVect){
-      //------ VARIABLES: initalizing ---------------------
-  string  fName;
-  string  lName;
-  int     id;
-  string  coName;
-  float   rate;
-  float   hours;
-  int     i = 0;                          //counter
-        //--- File: Opening and checking -------------------
-  fstream myFile;                         //opens file
-  myFile.open(myString);
+    string lName;
+    string fName;
+    float rate;
+    float hours;
+    int counter = 0;                        //counter that will save
+    string line;
 
-  if(!myFile) {                           //check if file open properly
-    cout << "Error: input.txt could not open. Closing Program." << endl;
+    fstream myFile;                         //opens file
+    myFile.open("data.txt");
+
+    if(!myFile) {                           //check if file open properly
+      cout << "Error: data.txt could not open. Closing Program." << endl;
+      exit(1);
+    }
+    myFile >> fName;
+    while(!myFile.eof()){
+      myFile >> lName;
+      myFile >> hours;
+      myFile >> rate;
+      myVector.emplace_back(fName, lName, rate, hours);      //Sets all values while looping
+      myFile >> fName;
+      counter++;
+    }
+
+    myFile.close();
+    return counter;
+}
+//-------------------------------------------------------------------------------------------
+void writeData(vector<Person> &myVector, int size){
+  //NEED TO NOW USE VECTORS INSTEAD OF ARRAY HER
+  //-------------------------------------------
+
+  ofstream myFile("Output.txt");
+
+  if(!myFile){                           //check if file open properly
     exit(1);
   }
-        //----- LOOP:Reading/Saving Data -----------------------------
-  myFile >> fName;
-  while(!myFile.eof()){
-    myFile >> lName;
-    myFile >> id;
-    myFile >> coName;
-    myFile >> hours;
-    myFile >> rate;
 
-    Person Temp;
-    myVect.push_back(Temp);
-    myVect[i].setFirstName(fName);
-    myVect[i].setLastName(lName);
-    myVect[i].setEmployeeId(id);
-    myVect[i].setCompanyName(coName);
-    myVect[i].setPayRate(rate);
-    myVect[i].setHoursWorked(hours);
-
-    myVect.push_back(Temp);
-    myFile >> fName;                   //use to check if reach (END OF FILE) for next loop
-    i++;
-    }
-
+  for(int i = 0; i < size; i++){         //Based on num of Employee, loops and print Employee's name and pay
+    myFile << myVector[i].fullName() << " "<< myVector[i].totalPay() << endl;
+  }
   myFile.close();
-};
-//-----------------------------------------------------------------------------------------------------------
-void getCompanies(vector<Person> &EmployeeVect, vector<string> &myCompanyVect){
-  int counter = 0;
-
-    //Loops through EmployeeVect and saves company of each member into a vector
-  while (counter < EmployeeVect.size()){
-    myCompanyVect.push_back(EmployeeVect[counter].getCompanyName());
-    counter++;
-  }
-
-    //sorts vector THEN erase duplicates to leave vector with only unique company names
-  sort(myCompanyVect.begin(), myCompanyVect.end());
-  myCompanyVect.erase(unique(myCompanyVect.begin(), myCompanyVect.end()), myCompanyVect.end());
-};
-//-----------------------------------------------------------------------------------------------------------
-void printHighestPaid(vector<Person> &EmployeeVect){
-        //------ VARIABLES: initalizing ---------------------
-  int    i = 0;
-  int    index = 0;
-  float  currentHighest = 0;
-
-        //------LOOP: saves an index of highest payed employee------
-  while(i < EmployeeVect.size()){
-    if(EmployeeVect[i].totalPay() > currentHighest){
-      currentHighest = EmployeeVect[i].totalPay();
-      index = i;
-    }
-    i++;
-  }
-        //---- COUT: highest payed employee's info ----
-  cout << "Highest paid: " << EmployeeVect[index].fullName() << endl;
-  cout << "Employee ID: " << EmployeeVect[index].getEmployeeId() << endl;
-  cout << "Employer: " << EmployeeVect[index].getCompanyName() << endl;
-  cout << "Total Pay: $" << fixed << setprecision(2) << currentHighest << endl;
-};
-//----------------------------------------------------------------------------------------------------------
-void seporateAndSave(vector<Person> &EmployeeVect, vector<string> &myCompanyVect)
-{
-    for(int i = 0; i < myCompanyVect.size(); i++)
-    {
-        string coFilenametxt = myCompanyVect[i] + ".txt";         //create string of compnay name + .txt
-        ofstream tempFile;
-        tempFile.open(coFilenametxt);                             //create company file and opens
-        tempFile << coFilenametxt << endl;
-        tempFile << "----------------------------------------------------------------------" << endl;
-
-        float theTotal = 0.0;
-        for(int j = 0; j < EmployeeVect.size(); j++)
-        {
-            if(EmployeeVect[j].getCompanyName() == myCompanyVect[i])
-            {
-                tempFile << setw(14) << left << EmployeeVect[j].getFirstName();
-                tempFile << setw(14) << left << EmployeeVect[j].getLastName();
-                tempFile << setw(4) << left << EmployeeVect[j].getEmployeeId();
-                tempFile << setw(12) << EmployeeVect[j].getCompanyName() << " ";
-                tempFile << "$" << fixed << setprecision(2) << EmployeeVect[j].totalPay() << endl;
-                theTotal = theTotal + EmployeeVect[j].totalPay();
-            }
-        }
-        tempFile << endl << "Total: $" << theTotal;
-        tempFile.close();
-    }
 }
 
-//-----------------------END OF FUNCTIONS/ MAINS STARTS HERE--------------------------------------
+//-----------------------------------------------------------------------------------------------
 
-main(){
+main() {
+  int EmployeeMax = 20;               //Maz size of array
+  vector<Person> myVector;       //creates array
 
-  vector <Person> employees;
-  vector <string> companyNames;
+  int numOfEmployee = readData(myVector, EmployeeMax);     //Returns num of existing Employees in file
 
-  readData("input.txt", employees);
-
-  getCompanies(employees, companyNames);
-
-  printHighestPaid(employees);
-
-  seporateAndSave(employees, companyNames);
+  writeData(myVector, numOfEmployee);                      //Will only print the amount of exisiting Employees
 
   return 0;
 }
